af47911934842db87ef09adf52c1537c2b712d87
tester <tester@tester.com>
2025-09-26 19:20:01 -0400

    revert cvar init from detour to dll startup

--- misc/client-extensions/ClientExtensions/Camera/CVar.cpp
+++ misc/client-extensions/ClientExtensions/Camera/CVar.cpp
@@ -15,10 +15,10 @@ enum CVarFlags : uint32_t {
 inline CVar* GetCVar(const char* name) { return ((decltype(&GetCVar))0x00767460)(name); }
 inline CVar* FindCVar(const char* name) { return ((decltype(&FindCVar))0x00767440)(name); }
 
-CLIENT_DETOUR(InitializeCVars, 0x00768340, __cdecl, void, (char* file))
-{
-    InitializeCVars(file);
+// CLIENT_DETOUR(InitializeCVars, 0x00768340, __cdecl, void, (char* file))
+// {
+//     InitializeCVars(file);
 
-    Spells::g_spell_min_clip_distance_percentage_cvar = CVar_C::Register("spellMinClipDistancePercentage", "Sets the minimum distance the clipping needs to be to activate", 1, "0.0", Spells::SpellMinClipDistancePercentage_CVarCallback, 5, 0, 0, 0);
-    MiscFixes::g_models_alpha_cvar = CVar_C::Register("modelFadeAlpha", "Alpha value for objects in camera LOS", 1, "0.1", MiscFixes::ModelsAlpha_CVarCallback, 5, 0, 0, 0);
-}
+//     Spells::g_spell_min_clip_distance_percentage_cvar = CVar_C::Register("spellMinClipDistancePercentage", "Sets the minimum distance the clipping needs to be to activate", 1, "0.0", Spells::SpellMinClipDistancePercentage_CVarCallback, 5, 0, 0, 0);
+//     MiscFixes::g_models_alpha_cvar = CVar_C::Register("modelFadeAlpha", "Alpha value for objects in camera LOS", 1, "0.1", MiscFixes::ModelsAlpha_CVarCallback, 5, 0, 0, 0);
+// }
