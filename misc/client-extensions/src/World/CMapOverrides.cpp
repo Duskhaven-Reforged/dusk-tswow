f68ab19e04138163b912eef99f29d0059614352e
tester <tester@tester.com>
2026-03-28 14:10:53 -0400

    pink cube

---
+++ misc/client-extensions/src/World/CMapOverrides.cpp
@@ -0,0 +1,20 @@
+#include <ClientDetours.h>
+#include <ClientLua.h>
+#include <FSRoot.h>
+#include <Logger.h>
+#include <SharedDefines.h>
+
+#include <Windows.h>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+CLIENT_DETOUR(CMap__SafeOpen, 0x007BD480, __cdecl, int, (char *Src, HANDLE* a2)) {
+    for (int i = 0; i < 10; i++) {
+        if (SFile::OpenFile(Src, a2)) {
+            return 1;
+        }
+    }
+    LOG_DEBUG << "BAD FILE READ, NO FIND " << Src;
+    return SFile::OpenFile("Spells\\ErrorCube.mdx",a2);
+}
